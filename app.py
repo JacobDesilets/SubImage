c4b15d81d4e4280b7c45c363c8052ce653e36c84
JacobDesilets <jacobdesilets@gmail.com>
2022-03-13 01:32:36 -0500

    Can now open images at correct size and navigate with scroll bars

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from PyQt6.QtCore import QSize
-from PyQt6.QtGui import QAction
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtGui import QAction, QPixmap, QImage, QPalette
 from pathlib import Path
 from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QToolBar, \
-    QFileDialog
+    QFileDialog, QScrollArea, QSizePolicy
 
 # Only needed for access to command line arguments
 import sys
@@ -18,26 +18,42 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+
+
+        open_action = QAction("&Open", self)
+        open_action.triggered.connect(self.open)
+
+        self.img_label = QLabel()
+        self.img_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.img_label.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Ignored)
+        self.img_label.setScaledContents(True)
+
+        self.img_label.setBackgroundRole(QPalette.ColorRole.Base)
+
+        toolbar = QToolBar("toolbar")
+        toolbar.addAction(open_action)
+
+        self.addToolBar(toolbar)
+
+
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setWidget(self.img_label)
+        self.scroll_area.setBackgroundRole(QPalette.ColorRole.Base)
+
+        self.setCentralWidget(self.scroll_area)
+
         self.setWindowTitle("My App")
         self.setMinimumSize(QSize(400, 300))
 
-        openAction = QAction("&Open", self)
-        openAction.triggered.connect(self.open)
-
-        img_label = QLabel()
-        toolbar = QToolBar("toolbar")
-        toolbar.addAction(openAction)
-
-        main_layout = QVBoxLayout()
-        self.addToolBar(toolbar)
-
-        widget = QWidget()
-        widget.setLayout(main_layout)
-        self.setCentralWidget(widget)
-
     def open(self):
         home_dir = str(Path.home())
-        f = QFileDialog().getOpenFileName(self, "Open File", home_dir, 'Images (*.png *.jpeg *.jpg *.bmp *.gif)')
+        f, _ = QFileDialog().getOpenFileName(self, "Open File", home_dir, 'Images (*.png *.jpeg *.jpg *.bmp *.gif)')
+        print(f)
+        img = QImage(f)
+        self.img_label.setPixmap(QPixmap.fromImage(img))
+        size = QSize(img.size())
+        self.img_label.setMinimumSize(size)
+
 
 window = MainWindow()
 window.show()
