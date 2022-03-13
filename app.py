b5235cb9dc8dfc5eda7c1dd7da120a2005d22b1b
JacobDesilets <jacobdesilets@gmail.com>
2022-03-13 16:47:37 -0400

    Added zoom functionality.

--- app.py
+++ app.py
@@ -18,23 +18,29 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-
+        self.original_pixmap = None
 
         open_action = QAction("&Open", self)
         open_action.triggered.connect(self.open)
+
+        zoom_in_action = QAction("Zoom &In", self)
+        zoom_in_action.triggered.connect(self.zoom_in)
+
+        zoom_out_action = QAction("Zoom &Out", self)
+        zoom_out_action.triggered.connect(self.zoom_out)
 
         self.img_label = QLabel()
         self.img_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.img_label.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Ignored)
         self.img_label.setScaledContents(True)
-
         self.img_label.setBackgroundRole(QPalette.ColorRole.Base)
 
         toolbar = QToolBar("toolbar")
         toolbar.addAction(open_action)
+        toolbar.addAction(zoom_in_action)
+        toolbar.addAction(zoom_out_action)
 
         self.addToolBar(toolbar)
-
 
         self.scroll_area = QScrollArea()
         self.scroll_area.setWidget(self.img_label)
@@ -51,8 +57,26 @@ class MainWindow(QMainWindow):
         print(f)
         img = QImage(f)
         self.img_label.setPixmap(QPixmap.fromImage(img))
+        self.original_pixmap = QPixmap.fromImage(img)
         size = QSize(img.size())
-        self.img_label.setMinimumSize(size)
+        self.img_label.resize(size)
+
+    def resize(self, scale):
+
+        old_pixmap = self.img_label.pixmap()
+        old_pixmap_size = old_pixmap.size()
+
+        new_pixmap = self.original_pixmap.scaled(int(old_pixmap_size.width() * scale), int(old_pixmap_size.height() * scale))
+
+        self.img_label.setPixmap(new_pixmap)
+
+        self.img_label.resize(self.img_label.pixmap().size())
+
+    def zoom_in(self):
+        self.resize(1.1)
+
+    def zoom_out(self):
+        self.resize(0.9)
 
 
 window = MainWindow()
